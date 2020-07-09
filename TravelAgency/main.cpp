fcca9fa174e7203a867b07225ebe3ac6014dc35c
Stishenok92 <Stishenok92@gmail.com>
2020-07-09 10:12:21 +0300

    Initial Commit

---
+++ TravelAgency/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TravelAgency
+//
+//  Created by Vadim Stishenok on 09.07.2020.
+//  Copyright © 2020 Vadim Stishenok. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
